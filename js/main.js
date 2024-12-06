3a18db6c0fccdda4d2f718225eee1ec660c8c6a3
Christian Burroughs <christianjburroughs@me.com>
2024-12-06 10:29:03 -0600

    update css

--- js/main.js
+++ js/main.js
@@ -50,12 +50,11 @@ function initialize() {
   highScore = parseInt(localStorage.getItem("highScore")) || 0;
   storedInitials = localStorage.getItem("highScoreInitials") || "";
 
-<<<<<<< HEAD
   gameWord =
     wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
-=======
+
   gameWord = wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
->>>>>>> main
+
 
   endGameDisplayEl.textContent = "";
   for (let button of letterButtons) {
